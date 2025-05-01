7302cc59a5d8c55c83876463975f15e0bd73415d
Baruch Gaxiola <baruch.gaxiola@gmail.com>
2025-05-01 17:44:21 -0500

    Entrenamiento offline (igual que antes)

--- online_logistic_regressor_freezable.py
+++ online_logistic_regressor_freezable.py
@@ -38,4 +38,41 @@ class LogisticRegression(nn.Module):
             # Congelar pesos seleccionados
             self.linear.weight.grad[:, self.freeze_mask] = 0.0
 
-# Cargar y 
+# Cargar y preparar datos (igual que antes)
+def load_data(filename):
+    df = pd.read_csv(filename)
+    X = df.drop('Outcome', axis=1).values
+    y = df['Outcome'].values
+    
+    X_temp, X_test, y_temp, y_test = train_test_split(
+        X, y, test_size=0.1, random_state=RANDOM_SEED
+    )
+    
+    X_offline, X_online, y_offline, y_online = train_test_split(
+        X_temp, y_temp, test_size=ONLINE_TRAIN_SIZE/(1-0.1), random_state=RANDOM_SEED
+    )
+    
+    scaler = StandardScaler()
+    X_offline = scaler.fit_transform(X_offline)
+    X_online = scaler.transform(X_online)
+    X_test = scaler.transform(X_test)
+    
+    return (X_offline, y_offline), (X_online, y_online), (X_test, y_test)
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, X, y):
+        self.X = torch.FloatTensor(X)
+        self.y = torch.FloatTensor(y).unsqueeze(1)
+        
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        return self.X[idx].to(device), self.y[idx].to(device)
+
+# Entrenamiento offline (igual que antes)
+def train_offline(model, criterion, optimizer, train_loader, epochs):
+    model.train()
+    losses = []
+    accuracies = []
+    
